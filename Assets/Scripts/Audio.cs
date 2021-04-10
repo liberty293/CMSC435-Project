4393d9037bf8de0fc6e12d9050b9c3f3564212a9
Joel <jvs@joelsamuel.com>
2021-04-10 16:52:32 -0400

    Adding Beat tracking

--- Assets/Scripts/Audio.cs
+++ Assets/Scripts/Audio.cs
@@ -4,25 +4,27 @@ using UnityEngine;
 
 public class Audio : MonoBehaviour
 {
+    //Bpm of our song Happy Trancun is 123 bpm
+    public float bpm = 123;
+    public float secPerBeat;
+    public float positionInSeconds;
+    public float positionInBeats;
+    public float timeStarted;
     public AudioSource music;
-    public bool startPlaying;
-    public Scroller beat;
     // Start is called before the first frame update
     void Start()
     {
-        
+        music = GetComponent<AudioSource>();
+        secPerBeat = 60f / bpm;
+        timeStarted = (float)AudioSettings.dspTime;
+        music.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!startPlaying)
-        {
-            if(Input.GetKeyDown("space"))
-            {
-                startPlaying = true;
-                music.Play();
-            }
-        }
-    }
+        positionInSeconds = (float)(AudioSettings.dspTime - timeStarted);
+        positionInBeats = positionInSeconds / secPerBeat;
+     }
+
 }
