ca4af13ff52cf4f5fefda794e06baae10ab14a15
liberty293 <40408116+liberty293@users.noreply.github.com>
2021-04-17 18:06:20 -0400

    Textured bear with animations and anim controller

---
+++ Assets/Scenes/Jump.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public Animator AnimControl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AnimControl.Play("Jump");
+        }
+        
+        if (Input.GetKeyDown(KeyCode.W)) AnimControl.Play("Roll");
+    }
+}
