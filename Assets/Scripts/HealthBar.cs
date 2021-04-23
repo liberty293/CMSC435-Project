4ac3d5817c67673fbb2de12ddd17e110bfbb6000
Joel <jvs@joelsamuel.com>
2021-04-22 22:22:50 -0400

    Adding Health Bar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+
+    public Slider slider;
+    public Gradient gradient;
+    public Image background;
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+
+        background.color = gradient.Evaluate(1);
+    }
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+        background.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
