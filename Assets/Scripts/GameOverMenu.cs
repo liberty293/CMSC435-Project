57656fc9369dfd7145997983605d9e11046813cf
Joel <jvs@joelsamuel.com>
2021-05-03 14:12:48 -0400

    Adding stuff to game over scene

---
+++ Assets/Scripts/GameOverMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public void RetryGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
