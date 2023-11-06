2d049dbdf7917d4bc89ae0f5cad0a0103b50603f
liberty293 <40408116+liberty293@users.noreply.github.com>
2023-11-05 19:37:20 -0800

    added project

--- Assets/Scripts/Obstacles/SyncAnimation.cs
+++ Assets/Scripts/Obstacles/SyncAnimation.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
-using UnityEditor.Animations;
 using UnityEngine;
 
 public class SyncAnimation : MonoBehaviour
